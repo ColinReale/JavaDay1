1e3c178b9f00889249b7194ea30057eb1f16144e
ColinReale <101147736+ColinReale@users.noreply.github.com>
2022-05-02 16:14:42 -0400

    first attempted commit

---
+++ MyNameIs.java
@@ -0,0 +1,8 @@
+class MyNameIs {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is");
+        System.out.println("Colin");
+        System.out.println("Reale");
+    }
+}
+
---
+++ Vacation.java
@@ -0,0 +1,8 @@
+class Vacation {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I'd like to visit");
+        System.out.println("Halifax");
+        System.out.println("Nova Scotia");
+    }
+}
+
